1be045f70352b0ee366c8c0b3693b69d4b425341
Cheonbi <lets.noriter@gmail.com>
2024-12-08 17:54:30 +0900

    feat : add new react training jsx - basic vite react jsx - App.jsx

---
+++ react-cheonbi/src/App.jsx
@@ -0,0 +1,94 @@
+import { useState } from 'react'
+import "./styles.css"
+import reactLogo from './assets/react.svg'
+
+const content = [
+    [
+        "React is extremely popular",
+        "It makes building complex, interactive UIs a breeze",
+        "It's powerful & flexible",
+        "It has a very active and versatile ecosystem"
+    ],
+    [
+        "Components, JSX & Props",
+        "State",
+        "Hooks (e.g., useEffect())",
+        "Dynamic rendering"
+    ],
+    [
+        "Official web page (react.dev)",
+        "Next.js (Fullstack framework)",
+        "React Native (build native mobile apps with React)"
+    ],
+    [
+        "Vanilla JavaScript requires imperative programming",
+        "Imperative Programming: You define all the steps needed to achieve a result",
+        "React on the other hand embraces declarative programming",
+        "With React, you define the goal and React figures out how to get there"
+    ]
+];
+
+function Header(){
+    return  (
+        <header className="App-header">
+            <h1 className="App-header__title">React Essentials</h1>
+            <p>Fundamental React concepts you will need for almost any app you are going to build</p>
+        </header>
+    );
+}
+
+function App() {
+    const [activeContentIndex, setActiveContentIndex] = useState(0);
+
+    return (
+        <div>
+            <header>
+                <img src={reactLogo} alt="React logo" />
+
+                <div>
+                    <h1>React.js</h1>
+                    <p>i.e., using the React library for rendering the UI</p>
+                </div>
+            </header>
+            <Header/>
+
+            <div id="tabs">
+                <menu>
+                    <button
+                        className={activeContentIndex === 0 ? "active" : ""}
+                        onClick={() => setActiveContentIndex(0)}
+                    >
+                        Why React?
+                    </button>
+                    <button
+                        className={activeContentIndex === 1 ? "active" : ""}
+                        onClick={() => setActiveContentIndex(1)}
+                    >
+                        Core Features
+                    </button>
+                    <button
+                        className={activeContentIndex === 2 ? "active" : ""}
+                        onClick={() => setActiveContentIndex(2)}
+                    >
+                        Related Resources
+                    </button>
+                    <button
+                        className={activeContentIndex === 3 ? "active" : ""}
+                        onClick={() => setActiveContentIndex(3)}
+                    >
+                        React vs JS
+                    </button>
+                </menu>
+                <div id="tab-content">
+                    <ul>
+                        {content[activeContentIndex].map((item) => (
+                            <li key={item}>{item}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App
